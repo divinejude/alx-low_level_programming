ffb53d0598e51d5fee65d1c55a2d37715f094bfc
Divine Okeke <okekedivinejude@gmail.com>
2023-06-16 10:26:11 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,21 @@
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints all single-digit numbers of base 10
- *              starting from 0, using putchar function.
+ * Description: Prints all single-digit numbers of base 10,
+ *              starting from 0, using printf function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number;
+    int number;
 
-	for (number = 0; number < 10; number++)
-	{
-		putchar(number + '0');
-	}
+    for (number = 0; number < 10; number++)
+    {
+        printf("%d\n", number);
+    }
 
-	putchar('\n');
-
-	return (0);
+    return 0;
 }
