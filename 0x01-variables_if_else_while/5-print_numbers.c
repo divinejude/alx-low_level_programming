f568eb92db2d453815a96a1c32ad0c12fb775a5b
Divine Okeke <okekedivinejude@gmail.com>
2023-06-16 10:04:58 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10
+ *              starting from 0, using putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
