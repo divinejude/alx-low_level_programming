299329fbc91e220aa8ffca0bfe8993b7462a6862
Divine Okeke <okekedivinejude@gmail.com>
2023-06-16 08:36:28 -0700

    0-positive_or_negative changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,10 @@
 /**
  * main - Entry point
  *
- * Code function: Assigns a random number to the v
- *              prints whether the number is positive, negative, or zero.
+ * Desc: Assigns a random number to the variable n and
+ *       prints whether the number is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,11 +16,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n<0)
+
+	if (n < 0)
 		printf("%d is negative\n", n);
-	else if (n>0)
+	else if (n > 0)
 		printf("%d is positive\n", n);
 	else
 		printf("%d is zero\n", n);
+
 	return (0);
 }
