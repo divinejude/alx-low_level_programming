899766bbaa951df5fd25b30ae34fdde5e574bfa2
Divine Okeke <okekedivinejude@gmail.com>
2023-06-20 07:46:07 -0700

    5-sign.c populated with code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_sign - check for the sign of a number
+ * @n: the number to be checked
+ * Return: 1 if positive and 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	return (n>0);
+}
