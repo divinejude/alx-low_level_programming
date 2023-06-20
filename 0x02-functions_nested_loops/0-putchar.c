8b1aca058903b7ec8997ba612230d04ef0d77c42
Divine Okeke <okekedivinejude@gmail.com>
2023-06-20 05:40:43 -0700

    change the 0-putchar.c file to suit the requirement of not using standard library

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point of the program
- *        Prints _putchar with a new line
+ * main - To print _putchar
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
