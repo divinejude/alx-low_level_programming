fd2f219fe343189f17342c8a82e485922782bda4
Divine Okeke <okekedivinejude@gmail.com>
2023-06-20 06:01:53 -0700

    Write code for task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet a-z with a newline
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
