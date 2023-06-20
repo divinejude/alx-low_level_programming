c8e7a2fb976749313f54b1ccb0b5a6cbc223e4d7
Divine Okeke <okekedivinejude@gmail.com>
2023-06-20 04:11:37 -0700

    add code for 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *        Prints _putchar with a new line
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
