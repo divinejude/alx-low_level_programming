79064bb7f7eab8ceac7e4c4dd34d8668b009874f
Divine Okeke <okekedivinejude@gmail.com>
2023-06-22 14:59:07 -0700

    10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int x, y, z;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= i; j++)
+		for (x = 0; x < size; x++)
 		{
-			_putchar('#');
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
