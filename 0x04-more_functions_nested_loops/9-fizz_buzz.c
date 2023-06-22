2bfa838f2b821ae78d8a397fd9c0e04dad6b8f8f
Divine Okeke <okekedivinejude@gmail.com>
2023-06-22 12:45:40 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,23 +2,34 @@
 
 /**
  * main - Entry point
- * Return: Return 0 on success
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
-		else
-			printf("%d ", i);
-	}
-	printf("\n");
-	return (0);
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return (0);
 }
