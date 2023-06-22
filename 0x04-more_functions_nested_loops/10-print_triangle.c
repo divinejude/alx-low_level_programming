0f4c0127acad7deffc5aca9721b918155f029323
Divine Okeke <okekedivinejude@gmail.com>
2023-06-22 13:02:30 -0700

    10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle using the character #
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
