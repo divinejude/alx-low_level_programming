8ff3b18abed85f5c10de7be8f8305b24cb9ef139
Divine Okeke <okekedivinejude@gmail.com>
2023-06-26 07:11:19 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,26 +6,25 @@
  * @n: Number of elements to be printed.
  *
  * Description: This function takes an input array and the number of elements
- *              to be printed. It iterates over the array and prints the
- *              elements using the printf function. Each element is separated
- *              by a comma and a space, except for the last element, which is
- *              printed without the comma and space. Finally, a new line is
- *              printed after all the elements.
+ * to be printed. It iterates over the array and prints the elements using the
+ * printf function. Each element is separated by a comma and a space, except
+ * for the last element, which is printed without the comma and space. Finally.
+ * a new line is printed after all the elements.
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < (n - 1); i++)
-    {
-        printf("%d, ", a[i]);
-    }
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
 
-    if (i == (n - 1))
-    {
-        printf("%d", a[n - 1]);
-    }
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
 
-    printf("\n");
+	printf("\n");
 }
 
